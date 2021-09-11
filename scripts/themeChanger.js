92b6fd8cb0b384a0e74e8b516ed3946f90227770
AngryMorrocoy <mrivasmahecha2003@gmail.com>
2021-09-11 06:28:24 -0400

    Now can remember using localStorage the previously selected theme

--- scripts/themeChanger.js
+++ scripts/themeChanger.js
@@ -1,13 +1,24 @@
-document.addEventListener('DOMContentLoaded', (_) => {
+const changeTheme = (newTheme) => {
+  const domBody = document.getElementsByTagName('body')[0];
+  domBody.classList.value = newTheme;
+
+  const respectiveRadioBtn = document.querySelector(`#${newTheme}`)
+  respectiveRadioBtn.checked = true
+
+  window.localStorage.setItem('prefered-theme', newTheme);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  let preferedTheme = window.localStorage.getItem('prefered-theme');
+  if (!preferedTheme) preferedTheme = 'theme1'
+  changeTheme(preferedTheme);
+
   const radioButtons = document.querySelectorAll('.radio-container > input');
 
   radioButtons.forEach((radioButton) => {
     radioButton.addEventListener('change', (evt) => {
       const newTheme = evt.target.id;
-
-      const domBody = document.getElementsByTagName('body')[0];
-
-      domBody.classList.value = newTheme;
+      changeTheme(newTheme);
     });
   });
 });
